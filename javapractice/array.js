853948b5ff8b5fc575a038bfcd952d3d5ab0af61
DurgaPrasad069 <durgaprasadveera2@gmail.com>
2024-04-24 11:55:32 +0530

    array method added

---
+++ javapractice/array.js
@@ -0,0 +1,59 @@
+// let array1 = [10,20,'rama',50]
+
+// //reading the values
+// let array2 = array1[0];
+// console.log (array2);
+
+// let array3 = array1 [2];
+// console.log (array3);
+// console.log(array1.length);
+// //using push method
+
+// array1.push(40);
+// console.log(array1);
+
+// //modify the value
+
+// array1[3]=40;
+// console.log(array1);
+
+// //using unshift method
+
+// array1.unshift (25);
+// console.log(array1);
+
+// //using pop method or removing
+
+// array1.pop();
+// console.log(array1);
+
+// //using shift method or removing
+
+// array1.shift();
+// console.log(array1);
+
+// //multi array
+
+//  const ar1= [
+//      ['sita',35,24],
+//      ['raju',46,68],
+//      ['ganga',24,76]
+//  ];
+ 
+let arr5= [10,20,30,40];
+
+// for(const ele of arr5){
+//     console.log(ele);
+// };
+
+let arr6= [10,20,30,40];
+
+for(let ele of arr6){
+    console.log(ele);
+};
+
+let arr7= [10,20,30,40];
+
+for(let i=0;i<arr7.length;i++){
+console.log(i);
+}
